41df7a24ebd4ec897460239f5801177061d3e014
macOS <macos@macOSs-MacBook-Pro.local>
2023-01-18 11:02:38 +0700

    Change algo since increase

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,57 @@
 import React, {useEffect, useState} from 'react';
-import {FetchUsersParams, ListRespone, UsersData} from "./interfaces/interfaces";
-import "./App.css";
-import {Button, Space, Spin} from "antd";
-import {useAppDispatch, useAppSelector} from "./app/hooks";
-import {fetchUser, getUsersState, resetUsersTable} from "./features/FetchUsers/FetchUsers.slice";
-import {UsersTable} from "./components";
+import {
+    FetchUsersParams,
+    ListRespone,
+    UsersData,
+} from './interfaces/interfaces';
+import './App.css';
+import {Button, Space, Spin} from 'antd';
+import {useAppDispatch, useAppSelector} from './app/hooks';
+import {
+    fetchUser,
+    getUsersState,
+    resetUsersTable,
+} from './features/FetchUsers/FetchUsers.slice';
+import {UsersTable} from './components';
 
 const App: React.FC = () => {
     const dispatch = useAppDispatch();
-    const {error, loading, usersData,lastId} = useAppSelector(getUsersState)
-    // const [fetchUsersParams, setFetchUsersParams] = useState<FetchUsersParams>({
-    //     since: 1,
-    //     per_page: 10,
-    // })
-    const handleLoadUsers = () => {
-        dispatch(fetchUser({since:lastId,per_page: 10}));
+    const {error, loading, usersData, lastId} = useAppSelector(getUsersState);
 
-    }
+    const handleLoadUsers = () => {
+        dispatch(fetchUser({since: lastId, per_page: 10}));
+    };
     const handleResetUsers = () => {
-        dispatch(resetUsersTable())
-    }
+        dispatch(resetUsersTable());
+    };
     return (
         <div className="App">
             <Space direction="vertical">
-                <Space direction="horizontal" align="center" size="small">
-                    <Button loading={loading} style={{width: "200px"}}
-                            onClick={() => handleLoadUsers()}>{usersData?.length === 0 ? 'Load user' : `Load more`}</Button>
-                    {usersData?.length !== 0 && <Button onClick={()=>handleResetUsers()}>Reset Users Table</Button>}
+                <Space
+                    direction="horizontal"
+                    align="center"
+                    size="small"
+                >
+                    <Button
+                        loading={loading}
+                        style={{width: '200px'}}
+                        onClick={() => handleLoadUsers()}
+                    >
+                        {usersData?.length === 0 ? 'Load user' : `Load more`}
+                    </Button>
+                    {usersData?.length !== 0 && (
+                        <Button onClick={() => handleResetUsers()}>
+                            Reset Users Table
+                        </Button>
+                    )}
                 </Space>
 
-                <UsersTable loading={loading} usersData={usersData}/>
+                <UsersTable
+                    loading={loading}
+                    usersData={usersData}
+                />
             </Space>
         </div>
-
     );
 };
 
--- src/features/FetchUsers/FetchUsers.slice.ts
+++ src/features/FetchUsers/FetchUsers.slice.ts
@@ -1,62 +1,63 @@
-import {createAsyncThunk, createSlice, PayloadAction} from "@reduxjs/toolkit";
-import usersApi from "../../api/usersApi";
-import {RootState} from "../../app/store";
-import {FetchUsersParams, UsersStateRedux} from "../../interfaces/interfaces";
-import {AxiosError} from "axios";
+import {createAsyncThunk, createSlice, PayloadAction} from '@reduxjs/toolkit';
+import usersApi from '../../api/usersApi';
+import {RootState} from '../../app/store';
+import {FetchUsersParams, UsersStateRedux} from '../../interfaces/interfaces';
+import {AxiosError} from 'axios';
 
 interface ValidationErrors {
-    errorMessage: string
-    field_errors: Record<string, string>
+    errorMessage: string;
+    field_errors: Record<string, string>;
 }
+
 export const fetchUser = createAsyncThunk(
-    "fetchUsers",
-    async ({since,per_page}:FetchUsersParams,thunkAPI) => {
-       try {
-           return  usersApi.getUsers({since, per_page});
-       } catch (err) {
-           // @ts-ignore
-           let error: AxiosError<ValidationErrors> = err // cast the error for access
-           if (!error.response) {
-               throw err
-           }
-           // We got validation errors, let's return those so we can reference in our component and set form errors
-           return thunkAPI.rejectWithValue(error.response.data.errorMessage)
-       }
+    'fetchUsers',
+    async ({since, per_page}: FetchUsersParams, thunkAPI) => {
+        try {
+            return usersApi.getUsers({since, per_page});
+        } catch (err) {
+            // @ts-ignore
+            let error: AxiosError<ValidationErrors> = err; // cast the error for access
+            if (!error.response) {
+                throw err;
+            }
+            // We got validation errors, let's return those so we can reference in our component and set form errors
+            return thunkAPI.rejectWithValue(error.response.data.errorMessage);
+        }
     }
-)
+);
 const initialState = {
-    error: "",
+    error: '',
     loading: false,
     usersData: [],
-    lastId:1,
+    lastId: 1,
 } as UsersStateRedux;
 const usersSlice = createSlice({
-    name: "UserSlice",
+    name: 'UserSlice',
     initialState,
     reducers: {
         resetUsersTable: (state) => {
             state.usersData = [];
             state.lastId = 1;
-        }
+        },
     },
     extraReducers: (builder) => {
-        builder.addCase(fetchUser.pending, (state,action) => {
+        builder.addCase(fetchUser.pending, (state, action) => {
             state.loading = true;
-        });builder.addCase(fetchUser.fulfilled, (state,action) => {
+        });
+        builder.addCase(fetchUser.fulfilled, (state, action) => {
             state.loading = false;
             // @ts-ignore
             state.usersData.push(...action.payload);
-            state.lastId = action.payload[action.payload.length - 1].id
-
-        });builder.addCase(fetchUser.rejected, (state,action) => {
+            state.lastId = action.payload[action.payload.length - 1].id;
+        });
+        builder.addCase(fetchUser.rejected, (state, action) => {
             state.loading = false;
             // @ts-ignore
             state.error = action.payload;
         });
-
-    }
-})
+    },
+});
 
 export default usersSlice.reducer;
 export const {resetUsersTable} = usersSlice.actions;
-export const getUsersState = (state:RootState) => state.users;
+export const getUsersState = (state: RootState) => state.users;
